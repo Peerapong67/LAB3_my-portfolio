2bc64ee56a8060b9908d7eb6545f051899d1ce29
Peerapong67 <pr3kubim@gmail.com>
2025-08-23 23:55:15 +0700

    Fix Deploy Page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/LAB3_my-portfolio/', // path ที่ต้องการ
+  base: './',
   plugins: [react()],
 });
